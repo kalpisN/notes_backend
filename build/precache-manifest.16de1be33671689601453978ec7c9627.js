92dcc593566825ca50fa69b7805c6503e962eaab
NooraKalpio <noora.kalpio@gmail.com>
2020-04-20 11:47:57 +0300

    build added

---
+++ build/precache-manifest.16de1be33671689601453978ec7c9627.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b313ab2775af698dba33ce28cc860de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a652f79b9a03c425cdc",
+    "url": "/static/js/2.a31c0208.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a31c0208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a8120681226ec33f3a",
+    "url": "/static/js/main.b732a371.chunk.js"
+  },
+  {
+    "revision": "80c66fa218045f49d32a",
+    "url": "/static/js/runtime-main.b8b2452e.js"
+  }
+]);
