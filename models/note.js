980d2ee649cfc2d829d9e45b695ed760e4c0507b
NooraKalpio <noora.kalpio@gmail.com>
2020-04-23 09:38:14 +0300

    .

--- models/note.js
+++ models/note.js
@@ -1,5 +1,6 @@
 
 const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
 
 const url = process.env.MONGODB_URI;
 
@@ -13,9 +14,16 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   })
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean
+    content: {
+        type: String,
+        minlength: 5,
+        required: true
+      },
+      date: { 
+        type: Date,
+        required: true
+      },
+      important: Boolean
 })
 
 const Note = mongoose.model('Note', noteSchema)
